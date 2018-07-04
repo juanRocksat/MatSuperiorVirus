7f69f43b87ad27af12186ace2281bf650977d71e
pacoDan <pacdaolmed@est.frba.utn.edu.ar>
2018-07-04 19:46:35 -0300

    con el modelo de ecuacion de TP

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -3,9 +3,8 @@ import algoritmos.Biseccion;
 import algoritmos.NewtonRaphson;
 import algoritmos.PuntoFijo;
 import datos.Datos;
-
 public class Principal {
-	public static void main(String[] args) {
+	public static void ejemplo() {
 		Datos ejemplo=new Datos(1,2,(double)1e-4) {
 			@Override
 			public double f(double x) {//f(x)
@@ -27,4 +26,27 @@ public class Principal {
 		newtonR.mostrarTabla_criterio2();
 		puntoFijo.mostrarTabla_criterio2();	
 	}	
+	public static void main(String[] args) {
+		Datos ejemplo=new Datos(20,21,(double)1e-5) {
+			@Override
+			public double f(double x) {//f(x)
+				return (double)((Math.pow(2, x)+25600*x-1571840));
+			}
+			@Override
+			public double f1(double x) {
+				return Math.pow(2, x)*Math.log(2)+25600; //derivada primera de f(x)			
+			}
+			@Override
+			public double g(double x) {
+				return 8+(1/Math.log(2))*(Math.log(6140-100*x));
+			}
+		};
+
+		Biseccion biseccion= new Biseccion(ejemplo);
+		NewtonRaphson newtonR = new NewtonRaphson(ejemplo,20.5);
+		PuntoFijo puntoFijo = new PuntoFijo(ejemplo,20.5);
+		biseccion.mostrarTabla_criterio2();
+		newtonR.mostrarTabla_criterio2();
+		puntoFijo.mostrarTabla_criterio2();	
+	}
 }
