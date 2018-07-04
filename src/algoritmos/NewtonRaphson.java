b2c20903ce039803bebca2978ddb00731b9ba3d2
pacoDan <pacdaolmed@est.frba.utn.edu.ar>
2018-07-03 22:40:25 -0300

    todos los algoritmos ok, falta tabla grafica

--- src/algoritmos/NewtonRaphson.java
+++ src/algoritmos/NewtonRaphson.java
@@ -12,13 +12,13 @@ public class NewtonRaphson extends Algoritmos{
 	public NewtonRaphson(Datos datos_ ,double x0_ ) {
 		this.datos=datos_;
 		this.x0=x0_;
+		this.cotaDeError=datos_.getCotaDeError();
 	}
 	@Override
 	public	void preparar() {
 		this.n=1;
-		this.xAnterior=this.x0;
 		this.xn=this.x0;
-		this.f_xn=this.datos.f(this.xn);
+		this.f_xn=this.f(this.xn);
 		
 	}
 	@Override
@@ -36,10 +36,19 @@ public class NewtonRaphson extends Algoritmos{
 	public	void correrConCriterioDeParo2() {
 		this.preparar();
 		this.registrarIteracion();
+		this.calcularProximo();
+		this.registrarIteracion();
 		while (this.criterioDeParo2()) {
-			calcularProximo();
-			registrarIteracion();
+			this.calcularProximo();
+			this.registrarIteracion();
 			}
+	}
+	@Override
+	public 	void calcularProximo() {
+		this.n++;
+		this.xAnterior=this.xn;
+		this.xn=this.xAnterior-(this.datos.f(xAnterior)/this.datos.f1(xAnterior));
+		this.f_xn=this.f(this.xn);
 	}
 	public double f(double x) {
 		return this.datos.f(x);
@@ -48,13 +57,7 @@ public class NewtonRaphson extends Algoritmos{
 		return this.datos.g(x);
 	}
 
-	@Override
-	public 	void calcularProximo() {
-		this.n++;
-		this.xAnterior=this.xn;
-		this.xn=this.xAnterior-(this.datos.f(xAnterior)/this.datos.f1(xAnterior));
-		this.f_xn=this.datos.f(this.xn);
-	}
+	
 
 	@Override
 	public 	ListaDeDatos getDatos() {
@@ -63,22 +66,21 @@ public class NewtonRaphson extends Algoritmos{
 	}
 	public static void main(String[] args) throws InterruptedException{
 		Principal p =new Principal();
-		Datos datos=new Datos((double)1,(double)2,(double)1e-3) {
+		Datos datos=new Datos((double)2,(double)2.1,(double)1e-4) {
 			@Override
 			public double f(double x) {
-				return (double)((Math.pow(Math.E, x))-4+x);
+				return (double)((Math.pow(x, 3)-2*x-5));
 			}
 			@Override
 			public double f1(double x) {
-				return (double)( Math.pow(Math.E, x)+1);
+				return (double)( Math.pow(x, 2)*3-2);
 			} //derivada primera	
 			@Override
 			public double g(double x) {
-				// TODO Auto-generated method stub
-				return 0;
+				return 0 ;
 			}
 		};
-		NewtonRaphson nR =new NewtonRaphson(datos, 1);
+		NewtonRaphson nR =new NewtonRaphson(datos, 2.1);
 //		try {
 //			biseccion.mostrarResultadoParaTest();
 //		} catch (Exception e) {
@@ -89,8 +91,10 @@ public class NewtonRaphson extends Algoritmos{
 		double x1 =datos.f1(1); 
 		p.saludar(Double.toString(x1));
 		nR.mostrarResultadoParaTest2();
-		p.saludar("Hola");p.saludar("Hola");
-			
+
+		p.saludar("Hola");p.saludar("criterio1 ");
+
+	
 		}
 	
 
